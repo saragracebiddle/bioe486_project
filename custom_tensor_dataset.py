baf2719af37f50949a1b05a0c3d0d6b55620b8a8
saragracebiddle <saragracebiddle@gmail.com>
2025-05-12 09:29:52 -0500

    CustomTensorDataset for dataLoader

--- custom_tensor_dataset.py
+++ custom_tensor_dataset.py
@@ -1,27 +1,48 @@
 # import the necessary packages
 from torch.utils.data import Dataset
-class CustomTensorDataset(Dataset):
+from plotting import generate_target
+import os
+import config
+
+def list_files_walk(start_path='.'):
+	out = []
+
+	for root, dirs, files in os.walk(start_path):
+		for file in files:
+			out.append(file)
+
+
+	return out
+
+class CustomTensorDataset(object):
 	# initialize the constructor
-	def __init__(self, tensors, transforms=None):
-		self.tensors = tensors
+	def __init__(self, transforms=None):
+		#self.tensors = tensors
 		self.transforms = transforms
+		self.imgs = list_files_walk(config.IMAGES_PATH)
 
 	def __getitem__(self, index):
 		# grab the image, label, and its bounding box coordinates
-		image = self.tensors[0][index]
-		label = self.tensors[1][index]
-		bbox = self.tensors[2][index]
+		#image = self.tensors[0][index]
+		#label = self.tensors[1][index]
+		#bbox = self.tensors[2][index]
+
 		# transpose the image such that its channel dimension becomes
 		# the leading one
-		image = image.permute(2, 0, 1)
+		#image = image.permute(2, 0, 1)
 		# check to see if we have any image transformations to apply
 		# and if so, apply them
-		if self.transforms:
-			image = self.transforms(image)
+		
 		# return a tuple of the images, labels, and bounding
 		# box coordinates
-		return (image, label, bbox)	
+		image, target = generate_target(index, 'mias_info/labels.txt')
+
+		if self.transforms is not None:
+		
+			image = self.transforms(image)
+
+		return image, target
 
 	def __len__(self):
 		# return the size of the dataset
-		return self.tensors[0].size(0)		
+		return len(self.imgs)	
---
+++ plotting.py
@@ -0,0 +1,95 @@
+from bs4 import BeautifulSoup
+import torch
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import pandas as pd
+import os
+import cv2
+from PIL import Image
+import numpy as np
+
+
+def generate_box(info, index, img):
+    (h, w) = np.array(img).shape[:2]
+
+    X = info['x'].iloc[index]
+    Y = info['y'].iloc[index]
+    R = info['r'].iloc[index]
+    if X == '*NOTE':
+        X = w //2
+        Y = h //2
+        R = w // 4
+    else:
+        X = float(X)
+        Y = float(Y)
+        R = float(R)
+        
+    centerX = float(X) / w
+    centerY = float(Y) / h
+    startX = centerX - (float(R)/w)
+    startY = centerY - (float(R/h))
+    endX = centerX + (float(R)/w)
+    endY = centerY + (float(R)/h)
+    
+    return [startX, startY, endX, endY]
+
+def generate_label(rows, r):
+    label = rows['severity'].iloc[0]
+    if label == "B":
+        return 1
+    elif label == "M":
+        return 2
+    return 0
+
+def generate_target(image_id, file):
+
+    with open(file) as f:
+        data = pd.read_csv(f,  sep = ' ', header = None)
+        data.columns = ['refno', 'tissue', 'class', 'severity','x','y','r']
+        data['severity'] = data['severity'].fillna('N')
+        data['image_id'] = data.refno.str.split('b', expand = True)[1].astype(int) -1
+
+        rows = data.loc[data['image_id'] == image_id]
+
+        imgPath = os.path.join('mias',rows['severity'].iloc[0], rows['refno'].iloc[0] + '.png')
+        img = Image.open(imgPath).convert('L')
+
+        # Bounding boxes for objects
+        # In coco format, bbox = [xmin, ymin, width, height]
+        # In pytorch, the input should be [xmin, ymin, xmax, ymax]
+        boxes = []
+        labels = []
+        for r in range(len(rows['refno'])):
+            boxes.append(generate_box(rows, r,img))
+            labels.append(generate_label(rows, r))
+        boxes = torch.as_tensor(boxes, dtype=torch.float32)
+        # Labels (In my case, I only one class: target class or background)
+        labels = torch.as_tensor(labels, dtype=torch.int64)
+        # Tensorise img_id
+        img_id = torch.tensor([image_id])
+        # Annotation is in dictionary format
+        target = {}
+        target["boxes"] = boxes
+        target["labels"] = labels
+        target["image_id"] = img_id
+        
+        return img, target
+    
+def plot_image(img_tensor, annotation):
+    
+    fig,ax = plt.subplots(1)
+    img = img_tensor.cpu().data
+
+    # Display the image
+    ax.imshow(img.permute(1, 2, 0))
+    
+    for box in annotation["boxes"]:
+        xmin, ymin, xmax, ymax = box
+
+        # Create a Rectangle patch
+        rect = patches.Rectangle((xmin,ymin),(xmax-xmin),(ymax-ymin),linewidth=1,edgecolor='r',facecolor='none')
+
+        # Add the patch to the Axes
+        ax.add_patch(rect)
+
+    plt.show()
